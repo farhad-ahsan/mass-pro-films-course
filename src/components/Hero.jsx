5a3bf92bc60779a7bad1585fde41e6c2930a2f84
farhad-ahsan <farhadahsan123@gmail.com>
2024-12-23 23:52:56 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -129,7 +129,7 @@ export default function Hero() {
               style={{
                 borderRadius: "10px",
               }}
-              src="https://www.youtube.com/embed/DfeQLjRoURY"
+              src="https://www.youtube.com/embed/PClYhfctcyw"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer;  gyroscope; picture-in-picture; web-share"
